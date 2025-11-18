5d39217f512521489cf3f01235209cc1eb927390
XinRC <cxin1017@gmail.com>
2025-11-18 02:26:50 -0500

    Modified smart_house.cpp

--- projects/modules/smart_house.cpp
+++ projects/modules/smart_house.cpp
@@ -4,83 +4,167 @@
 using namespace std;
 
 class Device { 
-    private:
-        string name;
-        string room;
-    public:
-        Device(const string& name, const string& room) 
-            : name(name), room(room) {}
-        const string& get_room() const { return room; }
-        const string& get_name() const { return name; }
+protected:
+    string name;
+    string id;
+public:
+    Device(const string& name, const string& id) : name(name), id(id) {}
+    virtual ~Device() = default;
+    
+    const string& get_id() const { return id; }
+    const string& get_name() const { return name; }
+    
+    // Virtual functions for capabilities
+    virtual bool canConnect() const = 0;
+    virtual bool canPowerManage() const = 0;
+    virtual bool isConnected() const { return false; }
+    virtual bool isPoweredOn() const { return false; }
+    virtual void connect() {}
+    virtual void disconnect() {}
+    virtual void turnOn() {}
+    virtual void turnOff() {}
+    
+    // Virtual display function
+    virtual void display() const = 0;
 };
 
 class Connectable {
-    private:
-        bool connected;
-    public:
-        Connectable() : connected(false) {}
-        // getter / setters
-        void set_connected(bool connect) { 
-            if (connect == true) { connected = true; } 
-            else { connected = false; }}
-            
+protected:
+    bool connected;
+public:
+    Connectable() : connected(false) {}
+    bool isConnected() const { return connected; }
+    void setConnected(bool status) { connected = status; }
 };
 
 class PowerManageable {
-    private:
-        bool turned_on;
-    public:
-        PowerManageable() : turned_on(false) {}
-        void set_turn_on(bool turn_on) {
-            if (turn_on == true) { turned_on = true;}
-            else { turned_on = false; }}
- };
+protected:
+    bool powered_on;
+public:
+    PowerManageable() : powered_on(false) {}
+    bool isPoweredOn() const { return powered_on; }
+    void setPoweredOn(bool status) { powered_on = status; }
+};
 
 class SmartLight : public Device, public Connectable, public PowerManageable {
-    public:
-        using Device::Device;
+public:
+    SmartLight(const string& name, const string& id) : Device(name, id) {}
+    
+    bool canConnect() const { return true; }
+    bool canPowerManage() const { return true; }
+    bool isConnected() const { return Connectable::isConnected(); }
+    bool isPoweredOn() const { return PowerManageable::isPoweredOn(); }
+    void connect() { setConnected(true); }
+   void disconnect()  { setConnected(false); }
+    void turnOn() { setPoweredOn(true); }
+    void turnOff() { setPoweredOn(false); }
+    
+    void display() const  {
+        cout << "SmartLight: " << name << " (ID: " << id << ") - "
+             << (isConnected() ? "Connected" : "Disconnected") << ", "
+             << (isPoweredOn() ? "Powered On" : "Powered Off");
+    }
 };
 
 class SmartOutlet : public Device, public PowerManageable {
-    public:
-        using Device::Device;
+public:
+    SmartOutlet(const string& name, const string& id) : Device(name, id) {}
+    
+    bool canConnect() const { return false; }
+    bool canPowerManage() const { return true; }
+    bool isPoweredOn() const { return PowerManageable::isPoweredOn(); }
+    void turnOn() { setPoweredOn(true); }
+    void turnOff() { setPoweredOn(false); }
+    
+    void display() const  {
+        cout << "SmartOutlet: " << name << " (ID: " << id << ") - "
+             << (isPoweredOn() ? "Powered On" : "Powered Off");
+    }
 };
 
 class SmartSpeaker : public Device, public Connectable {
-    public:
-        using Device::Device;
- };
+public:
+    SmartSpeaker(const string& name, const string& id) : Device(name, id) {}
+    
+    bool canConnect() const { return true; }
+    bool canPowerManage() const  { return false; }
+    bool isConnected() const { return Connectable::isConnected(); }
+    void connect() { setConnected(true); }
+    void disconnect() { setConnected(false); }
+    
+    void display() const {
+        cout << "SmartSpeaker: " << name << " (ID: " << id << ") - "
+             << (isConnected() ? "Connected" : "Disconnected");
+    }
+};
 
 class SmartHome {
-    private:
-        vector<Device*> smart_home_network; 
-    public: // displayAllDevices, connectDevice, turnOnDevice, displayAllDevices, disconnectAllDevices, turnoffDevice, 
-        SmartHome() {}
-        ~SmartHome() {
-            for (size_t i = 0; i < smart_home_network.size(); ++i) {
-                delete smart_home_network[i];
-            }
-            smart_home_network.clear();
+private:
+    vector<Device*> smart_home_network; 
+public:
+    void displayAllDevices() {
+        cout << "DEVICES:" << endl; 
+        for(size_t i = 0; i < smart_home_network.size(); ++i) {
+            cout << i + 1 << ". ";
+            smart_home_network[i]->display();
+            cout << endl;
         }
-        
-        void displayAllDevice() {
-            cout << "DEVICES: " << endl; 
-            for(Device* device : smart_home_network) {
+    }
 
-            }
-        }
+    void addDevice(Device* device) {
+        smart_home_network.push_back(device);
+    }
 
-        void addDevice(Device* device) {
-            smart_home_network.push_back(device);
-        }
-
-        void connectDevice(const string& room) {
-            for (Device* device : smart_home_network) {
-                if (room == device->get_room()) {
-                    cout << device->get_name() << 
-                }
+    void connectDevice(const string& device_id) {
+        for (Device* device : smart_home_network) {
+            if (device_id == device->get_id() && device->canConnect() && !device->isConnected()) {
+                device->connect();
+                cout << device->get_name() << " connected to WiFi." << endl;
+                return;
             }
         }
+        cout << "Device not found or cannot connect!" << endl;
+    }
+
+    void turnOnDevice(const string& device_id) {
+        for (Device* device : smart_home_network) {
+            if (device_id == device->get_id() && device->canPowerManage() && !device->isPoweredOn()) {
+                device->turnOn();
+                cout << device->get_name() << " turned ON." << endl;
+                return;
+            }
+        }
+        cout << "Device not found or cannot power manage!" << endl;
+    }
+
+    void disconnectDevice(const string& device_id) {
+        for (Device* device : smart_home_network) {
+            if (device_id == device->get_id() && device->canConnect() && device->isConnected()) {
+                device->disconnect();
+                cout << device->get_name() << " disconnected from WiFi." << endl;
+                return;
+            }
+        }
+        cout << "Device not found or not connected!" << endl;
+    }
+
+    void turnOffDevice(const string& device_id) {
+        for (Device* device : smart_home_network) {
+            if (device_id == device->get_id() && device->canPowerManage() && device->isPoweredOn()) {
+                device->turnOff();
+                cout << device->get_name() << " turned OFF." << endl;
+                return;
+            }
+        }
+        cout << "Device not found or not powered on!" << endl;
+    }
+
+    ~SmartHome() {
+        for (Device* device : smart_home_network) {
+            delete device;
+        }
+        smart_home_network.clear();
+    }
 };
 
 int main() {
@@ -117,38 +201,3 @@ int main() {
     
     return 0;
 }
-
-
-/*
-=== SMART HOME STATUS ===
-DEVICES:
-1. SmartLight: Living Room Light (ID: LR1) - Disconnected, Powered Off
-2. SmartOutlet: Kitchen Outlet (ID: K1) - Powered Off  
-3. SmartSpeaker: Bedroom Speaker (ID: B1) - Disconnected
-
-=== CONNECTING DEVICES ===
-Living Room Light connected to WiFi
-Bedroom Speaker connected to WiFi
-
-=== POWER MANAGEMENT ===
-Living Room Light turned ON
-Kitchen Outlet turned ON
-
-=== CURRENT STATUS ===
-DEVICES:
-1. SmartLight: Living Room Light (ID: LR1) - Connected, Powered On
-2. SmartOutlet: Kitchen Outlet (ID: K1) - Powered On
-3. SmartSpeaker: Bedroom Speaker (ID: B1) - Connected
-
-=== DISCONNECTING ===
-Bedroom Speaker disconnected from WiFi
-
-=== TURNING OFF ===
-Living Room Light turned OFF
-
-=== FINAL STATUS ===
-DEVICES:
-1. SmartLight: Living Room Light (ID: LR1) - Connected, Powered Off
-2. SmartOutlet: Kitchen Outlet (ID: K1) - Powered On
-3. SmartSpeaker: Bedroom Speaker (ID: B1) - Disconnected
-*/
