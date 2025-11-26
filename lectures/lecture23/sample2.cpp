6c0859b3c79146fe52a92c3e2fe8cb3f7abc556d
XinRC <cxin1017@gmail.com>
2025-11-26 13:10:29 -0500

    [/] .json, [+] sample2.cpp

---
+++ lectures/lecture23/sample2.cpp
@@ -0,0 +1,17 @@
+// Sample 2
+
+/*
+The .cpp will look templates
+*/
+#include <iostream>
+
+template <typename T>
+T add(T num1, T num2) {
+    cout << num1 + num2;
+    return num1 + num2; 
+}
+
+
+int main() {
+    int add(1, 2);
+}
