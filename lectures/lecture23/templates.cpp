162715ae7e8a62e4af7611ad86ecfc312c0cb99b
XinRC <cxin1017@gmail.com>
2025-12-08 23:16:02 -0500

    [\] templates.cpp

--- lectures/lecture23/templates.cpp
+++ lectures/lecture23/templates.cpp
@@ -1,5 +1,22 @@
 // Sample 2
 
 /*
-The .cpp will look into templates
+The .cpp will look into templates and their usage. Templates in C++ is very 
+similar to operator overloading for different types. However instead of 
+having different actions for each different type, no matter the type, the 
+action would be the same
 */
+
+#include <iostream>
+using namespace std;
+
+// creating the template
+template <typename T>
+void printingItem(T item) { cout << item << endl; }
+
+int main() {
+    printingItem(9.8);
+    printingItem("Word");
+    printingItem(6);
+    printingItem(false);
+}
